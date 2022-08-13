0ba1e280f9f35869216b91abb40cbbc83fc77ead
James Alhassan <jamesalhassan052@gmail.com>
2022-08-13 11:53:50 +0000

    name variable added and fgets function replaced one scanf

--- bmiCalculator.c
+++ bmiCalculator.c
@@ -8,15 +8,25 @@ int main()
     double Weight;
     double Height;
     double BMI;
+    char name[20];
 
+    printf("Please Enter Your Name: ");
+
+    fgets(name, 20, stdin);
 
     printf("Enter Your Weight in Kilograms:\n");
+
     scanf("%lf", &Weight);
+    
     printf("Enter Your Height in Metres/ Meters:\n");
+    
     scanf("%lf", &Height);
+    
     Height= Height * Height;
+    
     BMI = Weight /Height;
-    printf("Your BMI is %f\n", BMI);
+    
+    printf("Hi %sYour BMI is %f\n",name, BMI);
 
 
     return 0;
