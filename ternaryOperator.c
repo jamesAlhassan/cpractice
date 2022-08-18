99c9209d24d1b269a1cf845551831771f74ee5cc
James Alhassan <jamesalhassan052@gmail.com>
2022-08-18 20:52:53 +0000

    Ternary operator

---
+++ ternaryOperator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+int a= 100, b= 50, c;
+
+c= (a > b)? a : b;
+
+printf("if a is greater than b then c = a, else c = b. Ans: %d\n", c);
+
+
+  }
