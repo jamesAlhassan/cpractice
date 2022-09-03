ffbacce33af080d5336ef2437d8de7a1a1442f5d
James Alhassan <jamesalhassan052@gmail.com>
2022-09-03 14:53:46 +0000

    Structure of a C program-

--- structureOfCProgram.c
+++ structureOfCProgram.c
@@ -9,4 +9,23 @@ Topic: C Program Structure
 
 #include <stdio.h>
 
+//3. Definition Section
+#include MAX 100
 
+//4. Global Declaration Section
+int a =50;
+void display();
+
+//5. Main Section
+
+void main()
+{
+printf("Hello");
+display();
+
+}
+
+void display()
+{
+printf("THIS IS THE STRUCTUR OF A C PROGRAM!!!");
+}
