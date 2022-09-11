a8b2386e616d4c96215555587d84862a614ca4d5
James Alhassan <jamesalhassan052@gmail.com>
2022-09-11 08:36:46 +0000

    anwers to Hr 4 exsecises

---
+++ exerciseHr4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - answers to exercises of Hour 4
+ *
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+char B, y, e;
+B = 'B';
+y = 'y';
+e = 'e';
+
+printf("%c %c %c \n", B, y, e);
+
+return (0);
+}
