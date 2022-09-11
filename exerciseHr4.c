b706e71bf10131a9dfcc0449c69629ff27ab0973
James Alhassan <jamesalhassan052@gmail.com>
2022-09-11 09:03:36 +0000

    anwers to Hr 4 exsecises

--- exerciseHr4.c
+++ exerciseHr4.c
@@ -16,9 +16,12 @@ e = 'e';
 int num1 = 123;
 float num2 = 123.456;
 
-
+int num3 = 15;
+int num4 = 150;
+int num5 = 1500; 
 
 printf("%c %c %c \n", B, y, e);
 printf("%-d  %-f\n", num1, num2); // left align
+printf("%x %x %x\n", num3, num4, num5); // print nums in hex values
 return (0);
 }
