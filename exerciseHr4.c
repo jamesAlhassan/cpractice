7c0a645b98b7b253461b4c6ef01a9351970b6072
James Alhassan <jamesalhassan052@gmail.com>
2022-09-11 09:10:33 +0000

    anwers to Hr 4 exsecises

--- exerciseHr4.c
+++ exerciseHr4.c
@@ -20,8 +20,15 @@ int num3 = 15;
 int num4 = 150;
 int num5 = 1500; 
 
+int gett;
+printf("Enter a character: \n");
+gett = getchar();
+
 printf("%c %c %c \n", B, y, e);
 printf("%-d  %-f\n", num1, num2); // left align
 printf("%x %x %x\n", num3, num4, num5); // print nums in hex values
+printf("You entered:  ");
+putchar(gett);
+putchar('\n');
 return (0);
 }
