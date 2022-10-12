7f86087d9f4c0b1c7670ddd1ba5a4a4f4b0dfc93
James Alhassan <jamesalhassan052@gmail.com>
2022-10-12 19:31:10 +0000

    cal with function pointer updated

--- calFuncPtrs.c
+++ calFuncPtrs.c
@@ -14,13 +14,31 @@ int mod(int, int);
 
 int main(void)
 {
-	int num1, num2, operator;
+	int num1, num2, operator, result;
 
 	int (*ptAll[])(int, int) = {add, sub, mul, div, mod};
 
 	printf("Please enter two numbers \n");
-	scanf("%d%d", num1, num2);
-}
+	scanf("%d%d", &num1, &num2);
+
+	printf("Please enter operator:\n 0 for addition\n 1 for substraction\n 2 for multiplication\n 3 for division\n and 4 for modulo\n ");
+	scanf("%d", &operator);
+
+	if (operator < 0)
+		printf("Sorry, Operator: %d out of range\n", operator);
+
+	else if (operator > 4)
+		printf("Sorry, Operator: %d out of range\n", operator);
+
+	else
+	{
+		result = (*ptAll[operator])(num1, num2);
+	}
+
+	printf("Result: %d", result);
+
+	return (0);
+};
 
 int add(int x, int y)
 {
